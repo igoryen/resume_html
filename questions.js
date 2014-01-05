8a6d6e785d1b09c839007e09b36d78d7b13c156f
Yggy <igor.yentaltsev@gmail.com>
2014-01-05 14:46:32 -0500

    Added code for dynamic creation of a link to an element within the current page

--- questions.js
+++ questions.js
@@ -6,20 +6,21 @@ var starStoryDispenser = function(obj){
 	for (var no=1; no < Object.keys(obj).length; no++){
 		//document.getElementById(obj.name + no + "name").innerHTML = "aaaaaa";
 		
-		// dynamically lay the rails
-		var div_q = document.createElement("div"); // create a "div" element
+		// dynamically lay the rails for a question
+		var div_q = document.createElement("div"); // create a "div" element for a question
 		div_q.id = obj.name + no + "q"; // give it an "id"
 		div_q.className = "sub_main_item"; // give it a "class"
 		document.getElementById("questions").appendChild(div_q); // append it to div with id=questions
 		
-		var div_a = document.createElement("div"); // create a "div" element
+		// dynamically lay the rails for an answer
+		var div_a = document.createElement("div"); // create a "div" element for an answer
 		div_a.id = obj.name + no + "a"; // give it an "id"
-		div_a.className = "sub_main_item"; // give it a "class"
+		div_a.className = "padded"; // give it a "class"
 		document.getElementById("answers").appendChild(div_a); // append it to div with id=questions
 		
-		// send the train on the rails
-		document.getElementById(obj.name + no + "q").innerHTML = obj[no].q;
-		document.getElementById(obj.name + no + "a").innerHTML = obj[no].a;
+		// send the trains on the rails
+		document.getElementById(obj.name + no + "q").innerHTML = "<a href='#" + div_a.id + "'>" + obj[no].q + "</a>";
+		document.getElementById(obj.name + no + "a").innerHTML = obj[no].a + "<br><a href='#" + div_q.id + "'>Back to the question</a>";
 	}
 } // dispenser()
 
@@ -28,15 +29,15 @@ var starstories = {
 	name: "story",
 	1: {
 		q: "What are you most proud of in your career?",
-		a: "..." 
+		a: "The highlight of my career so far is ..." 
 	},
 	2: {
 		q: "What are you most proud of in each of your past positions?",
-		a: "..." 
+		a: "Of all the positions I had in the past, I think I am most proud of ... " 
 	},
 	3: {
 		q: "What challenge or crisis did you face on the job and what was your approach for solving each situation?",
-		a: "..." 
+		a: "Once it happened..." 
 	}
 };
 
